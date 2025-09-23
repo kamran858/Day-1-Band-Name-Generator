d33d040456bf5d820a2fa9ee5b5b921c24033828
Kamran Ashraf <kamranashraf@Mac.powerhub>
2025-09-23 21:38:47 +0100

    Initial commit

---
+++ task.py
@@ -0,0 +1,4 @@
+print("Wellcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\nw")
+print("Your band name could be " + city + " " + pet)
